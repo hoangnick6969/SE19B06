b4bed5f122f5cc3ee4636b75f6a771a2b8760a2b
NguyenVuHoang_DE190653 <hoangnguyennick@gmail.com>
2025-02-12 10:49:24 +0700

    1

---
+++ src/Tree/BinarySearchTree.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tree;
+
+/**
+ *
+ * @author PC
+ */
+public class BinarySearchTree {
+ /*
+    using Linked list to implement binary search tree
+    */   
+    class Node{
+        int data;
+        Node right, left;
+        public Node(int x){
+            this.data=x;
+            this.right=this.right=null;
+            
+        }
+    }
+    Node root;
+    public BinarySearchTree(){
+        this.root=null;
+    }
+    boolean isEmpty(){
+        return root==null;
+    }
+    void insert(int x){
+        if(isEmpty()) root=new Node(x);
+        Node temp=root;
+        Node temp1=null;
+        while(temp!=null){
+            temp1=temp;
+            if(temp.data>x) temp=temp.left;
+            else if(temp.data<x) temp=temp.right;
+            else return ;
+        }
+        if(temp1.data>x) temp1.left=new Node(x);
+        if(temp1.data<x) temp1.right=new Node(x);
+        
+    }
+    void preOrder(Node root){
+        if (isEmpty()) System.out.println("Empty");  
+        else{
+            System.out.println(root.data+ " ");
+            if (root.left != null) preOrder(root.left);
+            if (root.right != null) preOrder(root.right);
+        }
+    }
+    void preOrder(){
+        preOrder(root);
+    }
+    void inOrder(Node root){
+        if (isEmpty()) System.out.println("Empty");  
+        else{
+            
+            if (root.left != null) inOrder(root.left);
+            System.out.println(root.data+ " ");
+            if (root.right != null) inOrder(root.right);
+        }
+    }
+    void inOrder(){
+        inOrder(root);
+    }
+    void postOrder(Node root){
+       if (isEmpty()) System.out.println("Empty");  
+        else{
+            
+            if (root.left != null) postOrder(root.left);
+            if (root.right != null) postOrder(root.right);
+             System.out.println(root.data+ " ");
+        } 
+    }
+        void postOrder(){
+        postOrder(root);
+    }
+}
--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -10,19 +10,30 @@ package Tree;
  */
 public class Tree {
     public static void main(String[] args) {
-        ArrayBinaryTree aBinTree= new ArrayBinaryTree();
-        aBinTree.insert(3);
-        aBinTree.insert(5);
-        aBinTree.insert(7);
-        aBinTree.insert(9);
-        aBinTree.insert(6);
-        aBinTree.insert(4);
-        aBinTree.insert(2);
-        System.out.println("Pre-order traverse");
-        aBinTree.preOrder();
-        System.out.println("\nPost-order traverse");
-        aBinTree.postOrder();
-        System.out.println("\nIn-order traverse");
-        aBinTree.inOrder();
+//        ArrayBinaryTree aBinTree= new ArrayBinaryTree();
+//        aBinTree.insert(3);
+//        aBinTree.insert(5);
+//        aBinTree.insert(7);
+//        aBinTree.insert(9);
+//        aBinTree.insert(6);
+//        aBinTree.insert(4);
+//        aBinTree.insert(2);
+//        System.out.println("Pre-order traverse");
+//        aBinTree.preOrder();
+//        System.out.println("\nPost-order traverse");
+//        aBinTree.postOrder();
+//        System.out.println("\nIn-order traverse");
+//        aBinTree.inOrder();
+            BinarySearchTree bst=new BinarySearchTree();
+            bst.insert(10);
+            bst.insert(5);
+            bst.insert(3);
+            bst.insert(8);
+            bst.insert(20);
+            bst.insert(15);
+            System.out.println("Pre-order traverse: ");
+            bst.preOrder();
+             System.out.println("In-order traverse: ");
+            bst.inOrder();
     }
 }
